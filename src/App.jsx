37f0ecf7c605bf682eb5acb33d1a662825e124b5
GuyGinat <guyginat4@gmail.com>
2022-03-07 15:48:44 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        Test Application Page        
+      </header>
+    </div>
+  )
+}
+
+export default App
